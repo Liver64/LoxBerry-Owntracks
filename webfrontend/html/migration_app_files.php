6781b1b7ecf408172f00e547d79b0bf15398936f
Liver64 <olewald64@gmail.com>
2019-02-23 22:27:44 +0100

    tracking options added

--- webfrontend/html/migration_app_files.php
+++ webfrontend/html/migration_app_files.php
@@ -4,13 +4,16 @@ require_once "loxberry_system.php";
 
 $src = LBPHTMLAUTHDIR."/files/";
 $dest = LBPHTMLAUTHDIR."/files/user_app/";
-
+echo '<PRE>';
 $files = scandir($src);
+#var_dump($files);
 foreach($files as $file){
     if (is_file($src.$file)) {
         $rename_file = $dest.$file;
 		rename($src.$file, $rename_file);
-    }
+    } else {
+		continue;
+	}
 }
 $files_ren = scandir($dest);
 foreach($files_ren as $file1){
