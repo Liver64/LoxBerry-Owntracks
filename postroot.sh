e181ef6dbb9630e670c5ab547789f7a9d0564a4d
Liver64 <olewald64@gmail.com>
2025-03-23 10:19:06 +0100

    v1.0.4

--- postroot.sh
+++ postroot.sh
@@ -3,13 +3,30 @@
 
 if [ ! -d "REPLACELBPCONFIGDIR/recorder" ]; then
 	echo "<INFO> Recorder Installation starts"
+	source /etc/os-release
 	
-	curl https://raw.githubusercontent.com/owntracks/recorder/master/etc/repo.owntracks.org.gpg.key | sudo apt-key add -
-	echo "deb  http://repo.owntracks.org/debian bullseye main" | sudo tee /etc/apt/sources.list.d/owntracks.list > /dev/null
-	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-	sudo apt update
-	sudo apt install -y ot-recorder
-	
+	if [ $VERSION_ID == "10" ]; then
+		echo "<INFO> Recorder for Buster will be installed"
+		curl https://raw.githubusercontent.com/owntracks/recorder/master/etc/repo.owntracks.org.gpg.key | sudo apt-key add -
+		echo "deb  http://repo.owntracks.org/debian buster main" | sudo tee /etc/apt/sources.list.d/owntracks.list > /dev/null
+		sudo apt-get update
+		sudo apt-get install -y ot-recorder
+	fi
+
+	if [ $VERSION_ID == "11" ]; then
+		echo "<INFO> Recorder for Bullseye will be installed"
+		curl https://raw.githubusercontent.com/owntracks/recorder/master/etc/repo.owntracks.org.gpg.key | sudo apt-key add -
+		echo "deb  http://repo.owntracks.org/debian bullseye main" | sudo tee /etc/apt/sources.list.d/owntracks.list > /dev/null
+		sudo apt update
+		sudo apt install -y -recorder
+	fi
+	if [ $VERSION_ID == "12" ]; then
+		echo "<INFO> Recorder for Bookworm will be installed"
+		curl --no-progress-meter https://raw.githubusercontent.com/owntracks/recorder/master/etc/repo.owntracks.org.gpg.key | sudo tee /etc/apt/trusted.gpg.d/owntracks.asc
+		echo "deb  http://repo.owntracks.org/debian bookworm main" | sudo tee /etc/apt/sources.list.d/owntracks.list > /dev/null
+		sudo apt update
+		sudo apt install -y ot-recorder
+	fi
 	install -m444 /usr/share/doc/ot-recorder/ot-recorder.service /etc/systemd/system/ot-recorder.service
 	
 	#cp /REPLACELBPDATADIR/ot-recorder /etc/default
